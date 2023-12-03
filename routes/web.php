7a9a9e19a661a5268952dd93585ed1fd1b220f2e
Muhamad Husni <muhamad.husni@kemenkeu.go.id>
2023-12-03 11:33:45 +0700

    update versi

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(["app" => "Backend Duktek App", "Laravel Versi" => app()->version(), "PHP" => phpversion()]);
 });
